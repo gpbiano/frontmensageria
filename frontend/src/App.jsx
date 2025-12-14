098501bde9522a5d3455db6901234984c71a8bdc
Genivaldo Peres Biano <65170025+gpbiano@users.noreply.github.com>
2025-12-14 19:01:49 -0300

    atualiza error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -49,7 +49,7 @@ import "./styles/outbound.css";
 import "./styles/settings-groups.css";
 
 // ✅ CSS da página de canais
-import "../../styles/settings-channels.css";
+import "./styles/settings-channels.css";
 
 
 const AUTH_KEY = "gpLabsAuthToken";
